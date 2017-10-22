4c6d41d560ddb206f2e2d407e9af8b4bf824cf8a
Alexey Beall <alexeybeall@D-173-250-188-165.dhcp4.washington.edu>
2017-10-22 12:13:14 -0700

    Fixed authentication attempt #2

--- src/textToAudio.py
+++ src/textToAudio.py
@@ -6,9 +6,10 @@ import pyaudio
 
 def text_to_speech(answer):
     key = "1a7933ab6deb4875ae7fe8da33cd9eff"
+    key2 = "6304665b538349ec89e3a9cf57532202"
     jwt = requests.post("https://api.cognitive.microsoft.com/sts/v1.0/issueToken", headers={'Ocp-Apim-Subscription-Key': key}).text
     url = "https://speech.platform.bing.com/synthesize"
-    headers = {'X-Search-AppId': key, 'Authorization': jwt, 'X-Microsoft-OutputFormat': "audio-16khz-32kbitrate-mono-mp3"}
+    headers = {'X-Search-AppId': key, 'X-Search-ClientID':key2, 'Authorization': jwt, 'X-Microsoft-OutputFormat': "audio-16khz-32kbitrate-mono-mp3"}
     data = {'text': "<speak>" + answer + "</speak>"}
     r = requests.post(url, data=data, headers=headers)
     if(r.status_code >= 200 and r.status_code < 300):
